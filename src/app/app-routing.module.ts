05a56707fc7638cccdbd0f47eb257f1ae288ba21
yeissonLopez <42976704+yeissonLopez@users.noreply.github.com>
2020-05-31 13:36:05 -0500

    Organizar-proyecto-ps_creo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
 
 
 const routes: Routes = [
-  {path: 'home', component: HomeComponent}
+  {path: 'home', component: HomeComponent},
+  {path: 'signIn', component: SignInComponent},
+  {path: 'logIn', component: LogInComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { SignInComponent } from './pages/sign-in/sign-in.component';
+import { LogInComponent } from './pages/log-in/log-in.component';
+import { MyFooterComponent } from './components/my-footer/my-footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    SignInComponent,
+    LogInComponent,
+    MyFooterComponent
   ],
   imports: [
     BrowserModule,
